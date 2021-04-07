134a651a3e43ed13ec00833b7a7d422c4ce400a8
Yuji Ito <llamerada.jp@gmail.com>
2021-04-08 08:13:16 +0900

    update for libwebrtc m89.2
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ set -eu
 readonly ROOT_PATH=$(cd $(dirname $0) && pwd)
 
 ## Get OS environment parameters.
-if [ "$(uname -s)" = 'Darwin' ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     # Mac OSX
-    readonly ID='macos'
-    readonly ARCH='x86_64'
-    readonly IS_LINUX='false'
+    readonly ID="macos"
+    readonly ARCH="x86_64"
+    readonly IS_LINUX="false"
 
 elif [ -e /etc/os-release ]; then
     . /etc/os-release
     readonly ARCH=`uname -p`
-    readonly IS_LINUX='true'
+    readonly IS_LINUX="true"
 
 else
     echo "Thank you for useing. But sorry, this platform is not supported yet."
@@ -23,35 +23,33 @@ fi
 
 ## Download libwebrtc (Compiled chromium WebRTC native APIs.)
 readonly LOCAL_ENV_PATH=${ROOT_PATH}/local
-readonly WEBRTC_VER=m89
 
 mkdir -p ${LOCAL_ENV_PATH}/include
 mkdir -p ${LOCAL_ENV_PATH}/src
 cd ${LOCAL_ENV_PATH}/src
 
-# Filename
-if [ "${ID}" = 'macos' ]; then
-    readonly WEBRTC_FILE="libwebrtc-89.0.4389.114-macos-amd64.zip"
-else
-    readonly WEBRTC_FILE="libwebrtc-89.0.4389.114-linux-amd64.tar.gz"
-fi
-
 # Download and unarchive
-if ! [ -e "${WEBRTC_FILE}" ]; then
-    if [ "${ID}" = 'macos' ]; then
-	curl -OL https://github.com/llamerada-jp/libwebrtc/releases/download/${WEBRTC_VER}/${WEBRTC_FILE}
-	cd ${LOCAL_ENV_PATH}
-	unzip -o src/${WEBRTC_FILE}
-    else
-	wget https://github.com/llamerada-jp/libwebrtc/releases/download/${WEBRTC_VER}/${WEBRTC_FILE}
-	cd ${LOCAL_ENV_PATH}
-	tar zxf src/${WEBRTC_FILE}
+if [ "${ID}" = "macos" ]; then
+    readonly LIBWEBRTC_URL="https://github.com/llamerada-jp/libwebrtc/releases/download/m89/libwebrtc-89.0.4389.114-macos-amd64.zip"
+    readonly LIBWEBRTC_ARCHIVE="$(basename ${LIBWEBRTC_URL})"
+    if ! [ -e ${LIBWEBRTC_ARCHIVE} ]; then
+        curl -OL ${LIBWEBRTC_URL}
+        cd ${LOCAL_ENV_PATH}
+        unzip -o src/${LIBWEBRTC_ARCHIVE}
+    fi
+else
+    readonly LIBWEBRTC_URL="https://github.com/llamerada-jp/libwebrtc/releases/download/m89.2/libwebrtc-89.0.4389.114-linux-amd64.tar.gz"
+    readonly LIBWEBRTC_ARCHIVE="$(basename ${LIBWEBRTC_URL})"
+    if ! [ -e ${LIBWEBRTC_ARCHIVE} ]; then
+      wget ${LIBWEBRTC_URL}
+      cd ${LOCAL_ENV_PATH}
+      tar zxf src/${LIBWEBRTC_ARCHIVE}
     fi
 fi
 
 ## Build
 # Change compiler to clang on linux
-if [ "${IS_LINUX}" = 'true' ]; then
+if [ "${IS_LINUX}" = "true" ]; then
     export CC=cc
     export CXX=c++
 fi
